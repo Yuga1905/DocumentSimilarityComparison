cb1de33e7b313bff0b691c2ad394c4feed3d7cdd
2000078330 <2000078330@Evolve-VM-30>
2025-07-01 12:33:34 +0530

    Praveena changes

--- AgentHelper/ComparisonAgent.cs
+++ AgentHelper/ComparisonAgent.cs
@@ -1,27 +1,40 @@
 ﻿using DocumentSimilarityComparison.AzureHelper;
 using DocumentSimilarityComparison.DTO;
 using DocumentSimilarityComparison.Models;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DocumentSimilarityComparison.AgentHelper
 {
     public static class ComparisonAgent
     {
-        public static async Task<JobDescriptionDTO> MatchResumesWithJobDescription(string jobDescriptionPath)
+        public static async Task<JobDescriptionDTO> MatchResumesWithJobDescription(string jobDescriptionPath, string resumesFolderPath)
         {
             JobDescriptionDTO jobDescriptionDTO = new JobDescriptionDTO();
-            string folderPath = @"C:\Users\1000055632\source\repos\DocumentSimilarityComparison\DocumentSimilarityComparison\Resources\JobApplicantsResume";
-            string[] pdfFiles = Directory.GetFiles(folderPath, "*.pdf");
-            Job_Description_Model job_Description_Model=new Job_Description_Model();
-            (string resultJobDescriptionText, job_Description_Model) = await AzureHelper.AzureAIClientService.GetJobDescription(jobDescriptionPath, job_Description_Model);
+
+            // Ensure the folder exists
+            if (!Directory.Exists(resumesFolderPath))
+                return jobDescriptionDTO; // or throw error
+
+            string[] pdfFiles = Directory.GetFiles(resumesFolderPath, "*.pdf");
+
+            Job_Description_Model job_Description_Model = new Job_Description_Model();
+            (string resultJobDescriptionText, job_Description_Model) =
+                await AzureHelper.AzureAIClientService.GetJobDescription(jobDescriptionPath, job_Description_Model);
+
             foreach (string pdfPath in pdfFiles)
             {
-                ResumeDTO resumeDTO = new ResumeDTO();
-                resumeDTO.PdfPath = pdfPath;
+                ResumeDTO resumeDTO = new ResumeDTO
+                {
+                    PdfPath = pdfPath
+                };
                 await AzureHelper.AzureAIClientService.GetComparisonScoreAsync(pdfPath, resumeDTO, resultJobDescriptionText, job_Description_Model);
                 jobDescriptionDTO.Resumes.Add(resumeDTO);
             }
+
             jobDescriptionDTO.JdID = job_Description_Model.JdId;
             jobDescriptionDTO.JobTitle = job_Description_Model.JdTitle;
+
             return jobDescriptionDTO;
         }
     }
